5c1bd5c5793f526469a7b8ffd68038c19230f469
1451795172@qq.com <1451795172@qq.com>
2023-06-05 22:11:07 +0800

    111

--- lodash/xxj04-lodash.js
+++ lodash/xxj04-lodash.js
@@ -556,6 +556,55 @@ var xxj04 = {
     }
   }
   ,
-  
+  sortedLastIndexOf: function (array, value) {
+    var left = 0
+    var right = array.length - 1
+
+    while (left <= right) {
+      var mid = (left + right) >> 1
+
+      if (array[mid] > value) {
+        if (right == 0) {
+          return 0
+        }
+        right = mid - 1
+      }
+      else if (array[mid] <= value) {
+        left = mid + 1
+      }
+      else {
+        return mid
+      }
+    }
+    return right + 1
+  }
+  ,
+
+
+  sortedUniq: function (array) {
+    var a = new Set()
+    array.forEach((it) => {
+      a.add(it)
+    })
+    return [...a]
+
+  }
+
+
+  ,
+
+  sortedUniqBy: function (array, iteratee) {
+    var a = new Set()
+    var b = new Set()
+    array.forEach((it) => {
+      if (a.has(iteratee(it))) {
+        return
+      } else {
+        a.add(iteratee(it))
+        b.add(it)
+      }
+    })
+    return [...b]
+  }
 }
 
